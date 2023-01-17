455c06d67a0365548782514b59996b242117ab7f
Avaloness <98649348+Avaloness@users.noreply.github.com>
2023-01-17 12:58:34 +0300

    задание 2.4.6.8

---
+++ domashka.cs
@@ -0,0 +1,69 @@
+//Задача 2: Напишите программу,
+//которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+//Console.WriteLine("Введите первое число: ");
+//int FirstNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int SecondNumber = Convert.ToInt32(Console.ReadLine());
+//if(FirstNumber > SecondNumber)
+//{
+//Console.WriteLine("Первое число больше второго");
+//}
+//else if (FirstNumber < SecondNumber)
+//{
+//   Console.WriteLine("Первое число меньше второго");
+//}
+//else
+//{
+//    Console.WriteLine("Оба числа равны");
+//}
+//Console.ReadKey();
+
+
+//Задача 4: Напишите программу, 
+//которая принимает на вход три числа и выдаёт максимальное 
+//из этих чисел.
+
+//string[] i = Console.ReadLine().Split(' ');
+//Console.WriteLine("Введите первое число: ");
+//int FirstNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int SecondNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите третье число: ");
+//int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine( Math.Max(FirstNumber, Math.Max (SecondNumber, ThirdNumber)));
+
+
+//Задача 6: Напишите программу,
+// которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//int number;                             
+//Console.Write("введите число: ");
+//number= Console.Read();
+//if (number % 2==1)
+//{
+//    Console.WriteLine("число нечетное");         
+//}
+//else
+//{
+//    Console.WriteLine("Число четное");
+//}
+//Console.ReadKey();
+
+
+//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+//Console.Write("Введите первое число: ");
+//int n = int.Parse(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//int m = int.Parse(Console.ReadLine());
+//for (int i = n; i <= m; i++)
+//{
+//if (i % 2 == 0) 
+//{
+//    Console.Write(i + " ");
+//}
+//}
+ //   Console.ReadKey();
